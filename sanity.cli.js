12f913c713e2d762cfbd3962f409b0bf3cb464a8
rebs-code <jack.rebussi@gmail.com>
2024-05-23 15:42:28 +0200

    chore: Update sanity.cli.js to include project basePath and name
    This commit updates the sanity.cli.js file to include the project basePath and name. The basePath is set to '/studio' and the name is set to 'Neve Studio Blog'. This change allows for better configuration and identification of the project within the Sanity Studio.

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,8 +1,12 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'cpcy2oar',
     dataset: 'production'
-  }
+  },
+  project: {
+    basePath: '/studio',
+    name: 'Neve Studio Blog'
+  },
 })
